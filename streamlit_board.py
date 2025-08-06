2145088468e251d9123880b087f2f242ec79e766
PrethuRice <researchprethu@gmail.com>
2025-08-06 13:08:10 +0600

    Update streamlit_board.py

--- streamlit_board.py
+++ streamlit_board.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import joblib
+
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import StandardScaler
 
